247ff6bfebbaa5fe3ec16ec8744fad789ff2b33b
Elijah <125945721+Elijah-05@users.noreply.github.com>
2025-09-08 19:52:31 +0000

    interest calculator python instruction

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Interest calculator, 
+# Rate is the annual interest rate in 5% (0.05)
+# Time is 3 years
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
