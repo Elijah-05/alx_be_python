16670b615b6c998b53328112657b2ae2e68316af
Elijah <125945721+Elijah-05@users.noreply.github.com>
2025-10-03 08:58:14 +0000

    balance display amount update by showing in two decimal place

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -39,4 +39,4 @@ class BankAccount:
         # self.__account_balance -+ amount
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
